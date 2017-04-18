4f888ac18ae0f007f7af86cbd9b74b5c02461e41
pdinh_turner <phap.dinh@turner.com>
2017-04-18 15:17:57 -0400

    created basic webpage

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  //create browser window
+  win = new BrowserWindow({width: 800, height: 600});
+
+  //load index.html
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  //open devtools
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+//Quit when all windows are closed
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+});
