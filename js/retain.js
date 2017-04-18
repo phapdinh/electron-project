cb960c0a8644b6eba57ef7fbf3ce5f2d1fea0b97
pdinh_turner <phap.dinh@turner.com>
2017-04-18 15:27:19 -0400

    moved notes project into electron project

---
+++ js/retain.js
@@ -0,0 +1,92 @@
+$(function(){
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        },
+		clear: function() {
+			localStorage.notes = JSON.stringify([]);
+		}
+    };
+
+
+    var octopus = {
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr,
+				date: Date.now()
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes().reverse();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        },
+		deleteAll: function() {
+			model.clear();
+			view.render();
+		},
+        deleteOne: function(element) {
+            $(element).remove();
+            view.render();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+			var clearAll = $('#clear');
+            newNoteForm.submit(function(e){
+				if(newNoteContent.val()) {
+					octopus.addNewNote(newNoteContent.val());
+					newNoteContent.val('');
+					e.preventDefault();
+				};
+                $('#deleteOne').click(function(event) {
+                    $(event.currentTarget).parent().remove();
+                });
+            });
+			clearAll.click(function(e) {
+				octopus.deleteAll();
+				e.preventDefault();
+			});
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            octopus.getNotes().forEach(function(note){
+                htmlStr += '<li class="note">'+
+                        note.content +
+                    '<br/>' +
+					'<em class="note-date">'+
+					  new Date(note.date) +
+					'</em>'+
+                    '<button id="deleteOne">' +
+                    'delete' +
+                    '</button>' +
+					'</li>';
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
